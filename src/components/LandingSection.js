a588911172a3a8a8acebaa061674786ec2d462b8
ingila185 <ingila185@gmail.com>
2023-07-28 18:48:50 +0500

    fixed stuff

--- src/components/LandingSection.js
+++ src/components/LandingSection.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Avatar, Heading, VStack } from "@chakra-ui/react";
 import FullScreenSection from "./FullScreenSection";
+import ingila from '../images/ingila.jpg';
 
-const greeting = "Hello, I am Pete!";
-const bio1 = "A frontend developer";
-const bio2 = "specialised in React";
+const greeting = "Hello, I am Ingila!";
+const bio1 = "A full stack developer";
+const bio2 = "You can use this portfolio template for your UI/UX profile";
+const bio3 = "";
 
 // Implement the UI for the LandingSection component according to the instructions.
 // Use a combination of Avatar, Heading and VStack components.
@@ -17,7 +19,7 @@ const LandingSection = () => (
   >
   <VStack spacing={16}>
       <VStack spacing={4}>
-      <Avatar size="xl" name="Pete" src="https://i.pravatar.cc/150?img=7" />
+      <Avatar size="xl" name="Pete" src={ingila} />
       <Heading as="h6" size="sm" color="white">
         {greeting}
         </Heading>
@@ -26,7 +28,7 @@ const LandingSection = () => (
       <Heading as="h1" size="2xl" color="white">
         {bio1}
       </Heading>
-      <Heading as="h1" size="2xl" color="white">
+      <Heading as="h1" size="1xl" color="white">
         {bio2}
       </Heading>
       </VStack>
